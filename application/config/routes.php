7f06a03c664f8ebfb033d2b032bec800c8d43084
Murilo Wippel <murilowippel@gmail.com>
2018-10-30 19:55:57 -0300

    Alterações básicas para os prints do trabalho escrito

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,7 +50,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'dashboard';
-$route['404_override'] = '';
-$route['translate_uri_dashes'] = FALSE;
 $route['clientes/(:num)'] = 'clientes/editar/$1';
 $route['tiposcontratos/(:num)'] = 'tiposcontratos/editar/$1';
+
+$route['404_override'] = '';
+$route['translate_uri_dashes'] = FALSE;
